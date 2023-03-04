90a7188110875b311b2ba42f984f82b1e8550653
elizab22 <ebidwell04@gmail.com>
2023-03-03 23:32:49 -0600

    test

--- solarcore/App.js
+++ solarcore/App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import login from "./pages/login";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Explore from "./pages/Explore";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login"
+          component={login}
+        />
+        <Stack.Screen name="Home"
+          component={Home}
+        />
+        <Stack.Screen name="Profile"
+          component={Profile}
+        />
+        <Stack.Screen name="Explore"
+          component={Explore}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
