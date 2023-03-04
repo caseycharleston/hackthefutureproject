5d87cc2a1cd2ab60eea0bfcb7fa8359c4d4d0e96
caseycharleston <caseycharleston@gmail.com>
2023-03-04 14:00:21 -0600

    app.js config

--- solarcore/App.js
+++ solarcore/App.js
@@ -3,6 +3,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import Register from "./pages/Register";
 import Profile from "./pages/Profile";
 import Explore from "./pages/Explore";
 import Events from './pages/Events';
@@ -29,6 +30,26 @@ export default function App() {
         <Stack.Screen name="Profile"
           options={{headerShown: false}}
           component={Profile}
+        />
+        <Stack.Screen name="Explore"
+          options={{headerShown: false}}
+          component={Explore}
+        />
+        <Stack.Screen name="HTF"
+          options={{headerShown: false}}
+          component={HTF}
+        />
+        <Stack.Screen name="March"
+          options={{headerShown: false}}
+          component={March}
+        />
+          <Stack.Screen name="Leaderboard"
+          options={{headerShown: false}}
+          component={Leaderboard}
+        />
+          <Stack.Screen name="Events"
+          options={{headerShown: false}}
+          component={Events}
         />
       </Stack.Navigator>
     </NavigationContainer>
