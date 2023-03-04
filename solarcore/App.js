46f6cf5c6eba7b5277d2d5109562f39320a0eb1e
elizab22 <ebidwell04@gmail.com>
2023-03-04 13:49:10 -0600

    home renamed

--- solarcore/App.js
+++ solarcore/App.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Login from "./pages/Login";
-import Register from "./pages/Register"
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Explore from "./pages/Explore";
@@ -12,18 +11,18 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="Register"
-          component={Register}
-        />
+      <Stack.Navigator>
         <Stack.Screen name="Login"
           component={Login}
         />
-        {/* <Stack.Screen name="Home"
+        <Stack.Screen name="Home"
           component={Home}
-        /> */}
+        />
         <Stack.Screen name="Profile"
           component={Profile}
+        />
+        <Stack.Screen name="Explore"
+          component={Explore}
         />
       </Stack.Navigator>
     </NavigationContainer>
